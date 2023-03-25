6fd0efb684c3f4f3fd1c1de4f70af88879124e55
Tuan Nguyen <tuan.nguyen930708@gmail.com>
2023-03-25 11:05:53 +0700

    feat: add API refresh token

--- src/functions/auth/index.ts
+++ src/functions/auth/index.ts
@@ -1,5 +1,7 @@
 import login from "./login";
+import tokens from "./tokens";
 
 export default {
   "auth-login": login,
+  "auth-tokens": tokens,
 };
---
+++ src/functions/auth/tokens/handler.ts
@@ -0,0 +1,48 @@
+import { ProxyResult, Handler } from "aws-lambda";
+import middy from "@middy/core";
+import json, { Event } from "@middy/http-json-body-parser";
+import * as yup from "yup";
+import cfg from "@configs/index";
+import { ok } from "@libs/response";
+import * as mw from "@functions/middlewares";
+import {
+  InitiateAuthCommand,
+  InitiateAuthCommandInput,
+} from "@aws-sdk/client-cognito-identity-provider";
+import * as cognito from "@libs/cognito";
+
+const client = cognito.client(cfg.cognito);
+
+interface Req {
+  refresh_token: string;
+}
+
+const login: Handler<Event, ProxyResult> = async (event) => {
+  const req: Req = event.body as any;
+  const input: InitiateAuthCommandInput = {
+    ClientId: cfg.cognito.client.id,
+    AuthFlow: "REFRESH_TOKEN_AUTH",
+    AuthParameters: {
+      REFRESH_TOKEN: req.refresh_token,
+    },
+  };
+
+  const cmd = new InitiateAuthCommand(input);
+  const output = await client.send(cmd);
+
+  return ok(output);
+};
+
+export const main = middy()
+  .use(json())
+  .use(mw.logger.use())
+  .use(
+    mw.validator.use<Event>({
+      event: yup.object().shape({
+        body: yup.object().shape({
+          refresh_token: yup.string().required(),
+        }),
+      }),
+    })
+  )
+  .handler(login);
